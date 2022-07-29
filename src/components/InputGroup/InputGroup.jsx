f05d716b080a2b74cb18ed9bcfb4fb4d5f5bf9a4
Hatim Lagzar <lhatimdev@gmail.com>
2022-07-29 20:30:24 +0100

    remove focus effect and add onChange handler

--- src/components/InputGroup/InputGroup.jsx
+++ src/components/InputGroup/InputGroup.jsx
@@ -1,12 +1,22 @@
 import './InputGroup.scss';
 
-export default function InputGroup({ label, id, type }) {
+export default function InputGroup({
+  label,
+  id,
+  type,
+  handleInputChange = () => {},
+}) {
   return (
     <div className='input-group'>
       <label className='label-form' htmlFor={id}>
         {label}
       </label>
-      <input className='input-form' type={type} id={id} />
+      <input
+        className='input-form'
+        type={type}
+        id={id}
+        onChange={handleInputChange}
+      />
     </div>
   );
 }
