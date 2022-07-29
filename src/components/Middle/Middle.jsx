4233abc50813f322ffb18c4ca958680a46ce9bdf
Hatim Lagzar <lhatimdev@gmail.com>
2022-07-29 18:53:58 +0100

    rename variable

--- src/components/Middle/Middle.jsx
+++ src/components/Middle/Middle.jsx
@@ -5,13 +5,13 @@ import Menu from './../Menu/Menu';
 const Middle = (props) => {
   const isLoggedIn = authService.hasBeenAuthenticated();
 
-  let top_classes = isLoggedIn
+  let topClasses = isLoggedIn
     ? 'middle-top'
     : `${'middle-top'} ${'logged-out-spacer'}`;
 
   return (
     <div className={'middle'}>
-      <div className={top_classes}></div>
+      <div className={topClasses}></div>
       <div className={'middle-mid'}>
         <Menu />
         <div class={'content-right'}>{props.children}</div>
