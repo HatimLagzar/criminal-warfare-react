9a30781fb7ea07a97c7156d616c2887e96e503a4
Hatim Lagzar <lhatimdev@gmail.com>
2022-07-29 18:25:55 +0100

    add redux provider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider as ReduxProvider } from 'react-redux';
+
 import App from './components/App/App';
+import { store } from './app/store';
+import './scss/app.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ReduxProvider store={store}>
+        <App />
+      </ReduxProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
