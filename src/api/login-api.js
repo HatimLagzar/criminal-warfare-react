e07c8b60268cea48843264b1b5d8b39e0876cfba
Hatim Lagzar <lhatimdev@gmail.com>
2022-07-30 17:08:00 +0100

    use explicit hostname and port

--- src/api/login-api.js
+++ src/api/login-api.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 export const login = (formData) => {
-  return axios.post('/api/login', formData);
+  return axios.post('http://127.0.0.1:8000/api/login', formData);
 };
 
 export const refresh = (token) => {
   return axios.post(
-    '/api/refresh',
+    'http://127.0.0.1:8000/api/refresh',
     {},
     {
       headers: {
