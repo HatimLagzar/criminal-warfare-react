0083b4e7ea952b5eb35b1403107fdc34ed46577d
Hatim Lagzar <lhatimdev@gmail.com>
2022-07-31 12:53:49 +0100

    remove usage of props.styles.badge to print the classname

--- src/components/MenuLink/MenuBadge.jsx
+++ src/components/MenuLink/MenuBadge.jsx
@@ -2,9 +2,10 @@ const MenuBadge = (props) => {
   let id = 'badge-' + props.link;
 
   return (
-    <span className={props.styles.badge} data-count={props.badge} id={id}>
+    <span className={'badge'} data-count={props.badge} id={id}>
       {props.badge}
     </span>
   );
 };
+
 export default MenuBadge;
