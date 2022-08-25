6cf86b30d55e42a279a111d6bb2129d50b3edd96
Hatim Lagzar <lhatimdev@gmail.com>
2022-08-25 12:33:10 +0100

    change how data is extracted because api response has changed

--- src/components/OperationItem/OperationItem.jsx
+++ src/components/OperationItem/OperationItem.jsx
@@ -1,34 +1,22 @@
-import {formatMoney, formatNumber} from "../../utils/helpers/formatters";
-import {ucfirst} from "../../utils/helpers/strings";
-import Button from "../buttons/Button/Button";
+import ButtonForm from "../forms/ButtonForm/ButtonForm";
 
 export default function OperationItem({operation}) {
-  const type = ucfirst(operation.requirements.split('|')[0]);
-  const reward = parseInt(operation.requirements.split('|')[3]);
-  const rewardType = operation.requirements.split('|')[2];
-
   return <div className={'operation-item'}>
     <table className={'table'}>
       <thead>
       <tr>
-        <th>{type}</th>
-        <td>#{operation.tounlock}</td>
+        <th dangerouslySetInnerHTML={{__html: operation['title']}}></th>
+        <td>#{operation['tounlock']}</td>
       </tr>
       </thead>
       <tbody>
       <tr>
-        <td>{type} [x{operation.requirements.split('|')[1]}]</td>
-        {
-          parseInt(reward) > 0
-            ? <td className={'green-text'}>
-              [+{rewardType === 'money' || rewardType === 'bank' ? formatMoney(reward) : formatNumber(reward) + ' Points'}]
-            </td>
-            : <td>No Payouts</td>
-        }
+        <td dangerouslySetInnerHTML={{__html: operation['objectives']}}></td>
+        <td dangerouslySetInnerHTML={{__html: operation['pay']}}></td>
       </tr>
       <tr>
         <td colSpan={2}>
-          <Button text={'Start'} classes={'btn btn-link'}/>
+          <ButtonForm text={'Start'} classes={'btn btn-link'}/>
         </td>
       </tr>
       </tbody>
