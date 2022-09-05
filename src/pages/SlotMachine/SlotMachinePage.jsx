1a6e0490e1cfb71e60767ac18ac92fbb53474324
Hatim Lagzar <lhatimdev@gmail.com>
2022-09-05 16:55:07 +0100

    add spins left info to the slot machine page

--- src/pages/SlotMachine/SlotMachinePage.jsx
+++ src/pages/SlotMachine/SlotMachinePage.jsx
@@ -2,20 +2,32 @@ import ContentArea from "../../components/ContentArea/ContentArea";
 import {useEffect, useState} from "react";
 import InputGroup from "../../components/forms/InputGroup/InputGroup";
 import Button from "../../components/buttons/Button/Button";
-import {spinSlotMachine} from "../../api/dailies-api";
+import {getDailies, spinSlotMachine} from "../../api/dailies-api";
 import {useDispatch, useSelector} from "react-redux";
 import {setGeneralInfo} from "../../store/features/auth/authSlice";
 import toastr from "toastr";
 
 export default () => {
   const [isLoading, setIsLoading] = useState(false);
+  const [dailiesData, setDailiesData] = useState(null);
   const [bet, setBet] = useState(500);
-  const [turns, setTurns] = useState(1000);
+  const [spins, setSpins] = useState(1000);
   const dispatch = useDispatch();
   const generalInfo = useSelector(state => state.auth.generalInfo);
 
   useEffect(() => {
     document.title = 'Slot Machine | Criminal Warfare';
+
+    getDailies()
+      .then(response => {
+        setDailiesData(response.data.payload);
+      })
+      .catch(error => {
+        if (error.response)
+          toastr.error(error.response.data.message);
+
+        console.log(error);
+      })
   })
 
   function handleSubmit(e) {
@@ -23,14 +35,24 @@ export default () => {
 
     setIsLoading(true);
 
-    spinSlotMachine(bet, turns)
+    spinSlotMachine(bet, spins)
       .then(response => {
         setIsLoading(false);
-        toastr.success(response.data.message);
+
+        setDailiesData({
+          ...dailiesData,
+          "Slot Machine": {
+            ...dailiesData['Slot Machine'],
+            amount: dailiesData.dailies['Slot Machine'].amount + spins
+          }
+        })
+
         dispatch(setGeneralInfo({
           ...generalInfo,
           money: generalInfo.money + response.data.difference
         }));
+
+        toastr.success(response.data.message);
       })
       .catch(error => {
         setIsLoading(false);
@@ -42,16 +64,22 @@ export default () => {
       })
   }
 
+  if (dailiesData === null) {
+    return 'Loading...';
+  }
+
   return <div id={'slot-machine-page'}>
     <ContentArea title={'Slot Machine'}>
       <p className={'text-center'}>Enter the amount you would like to bet, how many turns you would like to use, then
         hit the Spin button!</p>
       <p className={'text-center'}>You are allowed to bet $500 per turn.</p>
-      <form style={{
-        width: '50%',
-        margin: '0 auto'
-      }}
-            onSubmit={handleSubmit}>
+      <form
+        onSubmit={handleSubmit}
+        style={{
+          width: '50%',
+          margin: '20px auto',
+        }}
+      >
         <InputGroup id={'betInput'}
                     label={'Bet'}
                     value={bet}
@@ -59,9 +87,12 @@ export default () => {
 
         <InputGroup id={'turnsInput'}
                     label={'Turns'}
-                    value={turns}
-                    handleInputChange={e => setTurns(parseInt(e.currentTarget.value))}/>
+                    value={spins}
+                    handleInputChange={e => setSpins(parseInt(e.currentTarget.value))}/>
+
         <Button isLoading={isLoading} showLoadingIcon={isLoading} text={'Spin'}/>
+
+        <span> You've {dailiesData.dailies['Slot Machine'].goal - dailiesData.dailies['Slot Machine'].amount} spins left.</span>
       </form>
     </ContentArea>
   </div>
