5c67aba07a9dd24548be2f06c456e69fc67df04b
Hatim Lagzar <lhatimdev@gmail.com>
2022-09-13 11:52:27 +0100

    remove usage of strings for authToken and authUser and use constants to change the name of the key easier

--- src/services/auth/AuthService.js
+++ src/services/auth/AuthService.js
@@ -1,11 +1,9 @@
-import { login, refresh } from '../../api/login-api';
+import {login, refresh} from '../../api/login-api';
 import jwtDecode from 'jwt-decode';
-import { setToken, setUser } from '../../store/features/auth/authSlice';
+import {setToken, setUser} from '../../store/features/auth/authSlice';
 import toastr from 'toastr';
-import {
-  SERVER_ERROR,
-  UNPROCESSABLE_ENTITY,
-} from '../../utils/constants/response-codes';
+import {SERVER_ERROR, UNPROCESSABLE_ENTITY,} from '../../utils/constants/response-codes';
+import {TOKEN_LOCALSTORAGE_KEY_NAME, USER_LOCALSTORAGE_KEY_NAME} from "../../utils/constants/global";
 
 class AuthService {
   login(username, password) {
@@ -18,8 +16,8 @@ class AuthService {
         const token = response.data.token;
         const user = jwtDecode(token);
 
-        localStorage.setItem('authToken', token);
-        localStorage.setItem('authUser', JSON.stringify(user));
+        localStorage.setItem(TOKEN_LOCALSTORAGE_KEY_NAME, token);
+        localStorage.setItem(USER_LOCALSTORAGE_KEY_NAME, JSON.stringify(user));
 
         toastr.success(response.data.message);
 
@@ -66,8 +64,8 @@ class AuthService {
   }
 
   logout(dispatch = null) {
-    localStorage.removeItem('authToken');
-    localStorage.removeItem('authUser');
+    localStorage.removeItem(TOKEN_LOCALSTORAGE_KEY_NAME);
+    localStorage.removeItem(USER_LOCALSTORAGE_KEY_NAME);
 
     if (dispatch !== null) {
       dispatch(setToken(null));
@@ -76,7 +74,7 @@ class AuthService {
   }
 
   getAuthUser() {
-    return JSON.parse(localStorage.getItem('authUser'));
+    return JSON.parse(localStorage.getItem(USER_LOCALSTORAGE_KEY_NAME));
   }
 
   /**
@@ -84,8 +82,8 @@ class AuthService {
    */
   hasBeenAuthenticated() {
     return (
-      localStorage.getItem('authToken') !== null &&
-      localStorage.getItem('authUser') !== null
+      localStorage.getItem(TOKEN_LOCALSTORAGE_KEY_NAME) !== null &&
+      localStorage.getItem(USER_LOCALSTORAGE_KEY_NAME) !== null
     );
   }
 
@@ -107,7 +105,7 @@ class AuthService {
   }
 
   getToken() {
-    return localStorage.getItem('authToken');
+    return localStorage.getItem(TOKEN_LOCALSTORAGE_KEY_NAME);
   }
 
   getUser() {
@@ -115,7 +113,7 @@ class AuthService {
       return null;
     }
 
-    return JSON.parse(localStorage.getItem('authUser'));
+    return JSON.parse(localStorage.getItem(USER_LOCALSTORAGE_KEY_NAME));
   }
 
   refreshToken(token) {
@@ -139,7 +137,7 @@ class AuthService {
    * @param {fn} dispatch
    */
   saveToken(token, dispatch) {
-    localStorage.setItem('authToken', token);
+    localStorage.setItem(TOKEN_LOCALSTORAGE_KEY_NAME, token);
 
     const user = jwtDecode(token);
 
--- src/utils/constants/global.js
+++ src/utils/constants/global.js
@@ -1,1 +1,4 @@
 export const CDN_URL = 'https://media.criminal-warfare.com/';
+
+export const TOKEN_LOCALSTORAGE_KEY_NAME = 'cw_auth_token';
+export const USER_LOCALSTORAGE_KEY_NAME = 'cw_auth_user';
